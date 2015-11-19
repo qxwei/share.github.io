a41e69991eab22bdeb6b081d82063c76e5057cd7
Damon <121440270@qq.com>
2015-11-19 23:16:44 +0800

    1223

--- chat123/resources/scripts/app/controllers/HallController.js
+++ chat123/resources/scripts/app/controllers/HallController.js
@@ -390,7 +390,7 @@ MetronicApp.controller('HallController', function($rootScope, $scope, $http, $ti
     	{
     		console.info('enterNewSession fail,try agian.');
     		console.error(err);
-    	//	enterNewSession(isLoadHistory,isInform)
+    		$timeout(function(){enterNewSession(isLoadHistory,isInform);}, 4000);
     	}
     }
     //处理会话切换
