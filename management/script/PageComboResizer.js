fd7a402354a2113a67e32aff48076999d36bb315
Damon <121440270@qq.com>
2015-11-25 08:21:55 +0800

    management static resource

---
+++ management/script/PageComboResizer.js
@@ -0,0 +1,73 @@
+Ext.namespace('Ext.ux.plugins');
+
+Ext.ux.plugins.PageComboResizer = Ext.extend(Object, {
+    pageSizes: [5, 10, 15, 20, 25, 30, 50, 75, 100, 200, 300, 500],
+    prefixText: 'Showing ',
+    postfixText: 'records per page.',
+    constructor: function (config) {
+        Ext.apply(this, config);
+        Ext.ux.plugins.PageComboResizer.superclass.constructor.call(this, config);
+    },
+    init: function (pagingToolbar) {
+        var ps = this.pageSizes;
+        var combo = new Ext.form.ComboBox({
+            typeAhead: true,
+            triggerAction: 'all',
+            lazyRender: true,
+            mode: 'local',
+            width: 60,
+            store: ps,
+            listeners: {
+                afterrender: function (self, eOpts) {
+                    self.setValue(ps[0]);
+                },
+                select: function (c, r, i) {
+
+                    pagingToolbar.pageSize = c.getValue( );
+                    var rowIndex = 0;
+                    var gp = pagingToolbar.findParentBy(
+                            function (ct, cmp) {
+                                return (ct instanceof Ext.grid.GridPanel) ? true : false;
+                            }
+                    );
+                    var sm = gp.getSelectionModel();
+                    if (undefined != sm && sm.hasSelection()) {
+                        if (sm instanceof Ext.grid.RowSelectionModel) {
+                            rowIndex = gp.store.indexOf(sm.getSelected());
+                        } else if (sm instanceof Ext.grid.CellSelectionModel) {
+                            rowIndex = sm.getSelectedCell()[0];
+                        }
+                    }
+                    rowIndex += pagingToolbar.cursor;
+
+                    pagingToolbar.store.pageSize = Ext.Number.from(c.getValue( ), 10);
+                    pagingToolbar.store.load({
+                        params: {
+                            start: 0,
+                            limit: Ext.Number.from(c.getValue( ), 10)
+                        }
+                    });
+                }
+            }
+        });
+
+        Ext.iterate(this.pageSizes, function (ps) {
+            if (ps == pagingToolbar.pageSize) {
+                combo.setValue(ps);
+                return;
+            }
+        });
+
+        var inputIndex = pagingToolbar.items.indexOf(pagingToolbar.refresh);
+        pagingToolbar.insert(++inputIndex, '-');
+        pagingToolbar.insert(++inputIndex, this.prefixText);
+        pagingToolbar.insert(++inputIndex, combo);
+        pagingToolbar.insert(++inputIndex, this.postfixText);
+        pagingToolbar.on({
+            beforedestroy: function () {
+                combo.destroy();
+            }
+        });
+
+    }
+});
